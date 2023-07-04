86dbf36f4f0ca13cb9acc832f34b9ed057d0bb00
ok-qa <olechka.kr@gmail.com>
2023-07-04 05:23:08 -0700

    player

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('#vimeo-player');
+const player = new Vimeo.Player(iframe);
+const timeKey = 'videoplayer-current-time';
+
+
+    player.on('timeupdate',throttle(saveThrottledTime) );
+
+
+function saveThrottledTime() { 
+    player.currentTime().then(function (seconds){ 
+        localStorage.setItem(timeKey, seconds);
+    })
+}
+
+
+  const savedTime = localStorage.getItem(timeKey);
+  if (savedTime) {
+    player.setCurrentTime(savedTime);
+  }
 
